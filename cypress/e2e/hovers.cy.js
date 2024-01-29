6d032ebf5d071b525720dc64c8c697d41d874420
Vanya <vanyaberezin1404@gmail.com>
2024-01-29 18:05:46 +0200

    added hover page tests

---
+++ cypress/e2e/hovers.cy.js
@@ -0,0 +1,20 @@
+describe('Hovers', () => {
+
+    beforeEach(() => {
+        cy.visit('/')
+        cy.get('[href="/hovers"]').click()
+    });
+
+    it('Should verify hovering over user icons',  function () {
+        cy.get('.figcaption').each(usernameElem => {
+            cy.wrap(usernameElem.children('h5')).should('not.be.visible')
+            cy.wrap(usernameElem.children('a')).should('not.be.visible')
+        })
+        cy.get('.figure').each(icon => {
+            cy.wrap(icon).realHover('mouse').then((iconElem) => {
+                cy.wrap(iconElem.find('h5')).should('be.visible').and('contain', 'name: user')
+                cy.wrap(iconElem.find('a')).should('be.visible').and('have.text', 'View profile')
+            })
+        })
+    });
+});
