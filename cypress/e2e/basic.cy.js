07ad1392d4ee3cee601c88a0068a95c780c7c066
Vanya <vanyaberezin1404@gmail.com>
2024-01-29 14:15:19 +0200

    updated auth test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,8 +1,10 @@
-
-
-describe('Suite1', () => {
-
-   it('Test case 1', () => {
-      cy.visit("");
+describe('Form Authentication', () => {
+   it('Should verify inability to login with invalid credentials',  function () {
+      cy.visit('https://the-internet.herokuapp.com')
+      cy.get('[href="/login"]').click()
+      cy.get('#username').type('testUser123123')
+      cy.get('#password').type('testPassword12313')
+      cy.get('[type="submit"]').click()
+      cy.get('.flash').should('contain', 'Your username is invalid!')
    });
 });
